b707770f046150a8620ab4c5b2233f254ecb5945
hidekiti42 <166455038+hidekiti42@users.noreply.github.com>
2025-12-24 19:25:01 +0900

    7章課題

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP7章課題</title>
+</head>
+<body>
+  <?php
+   // 連想配列の値を代入する
+    $product_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+   // 連想配列の値を出力する
+    print_r($product_data);
+    ?>
+</body>
+</html>
