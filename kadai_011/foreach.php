121954f640ba6a72d8d762b9326673310f19ec77
hidekiti42 <166455038+hidekiti42@users.noreply.github.com>
2026-01-01 21:14:20 +0900

    11章課題

---
+++ kadai_011/foreach.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+initial-scale=1.0">
+  <title>11章課題</title>
+</head>
+<body>
+  <p>
+      <?php
+      $vegetables = [
+            '名前' => '玉ねぎ',
+            '値段' => 200,
+            '産地' => '北海道'
+      ];
+
+      foreach ($vegetables as $key => $value) {
+        echo "{$key} : {$value}<br>";
+      }
+      ?>
+  </p>
+</body>
+</html>
